6739b670aae6d21780b0cf1609302c0d25f5d322
Amy Thompson <amythompson9@gmail.com>
2018-09-16 20:39:11 -0400

    Start Refactoring

--- ColorGameUdemy/JavaScript.js
+++ ColorGameUdemy/JavaScript.js
@@ -6,39 +6,78 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
+//REFACTORING getting rid of indiviual ID for one class - see above
+//var easyBtn = document.querySelector("#easyBtn");
+//var hardBtn = document.querySelector("#hardBtn");
 
-
-easyBtn.addEventListener("click", function () {
-    hardBtn.classList.remove("selected");
-    easyBtn.classList.add("selected");
-    numSquares = 3;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for (var i = 0; i < squares.length; i++) {
-        if (colors[i]) {
-            squares[i].style.backgroundColor = colors[i];
+for (var i = 0; i < modeButtons.length; i++) {
+    modeButtons[i].addEventListener("click", function () {
+        modeButtons[0].classList.remove("selected");
+        modeButtons[1].classList.remove("selected");
+        this.classList.add("selected");
+        if (this.textContent === "Easy") {
+            numSquares = 3;
         }
         else {
-            squares[i].style.display = "none";
+            numSquares = 6;
         }
-    }
-});
+        reset();
 
-hardBtn.addEventListener("click", function () {
-    hardBtn.classList.add("selected");
-    easyBtn.classList.remove("selected");
-    numSquares = 6;
+        //to do everything below, we are going to make the "reset function"
+        //figure out how many squares to show
+        //pick new colors
+        //pick a new pickedColor
+        //update page to reflect changes
+    });
+}
+
+function reset() {
+    //generate all new colors
     colors = generateRandomColors(numSquares);
+    //pick new random color from array
     pickedColor = pickColor();
+    //change colorDisplay to match picked Color
     colorDisplay.textContent = pickedColor;
+    resetButton.textContent = "New Colors";
+    messageDisplay.textContent = "";
+    //change colors from squares
     for (var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
-        squares[i].style.display = "block";
     }
-});
+    h1.style.backgroundColor = "steelblue";
+}
+
+//REFACTORING getting rid of indiviual ID for one class - see above
+//easyBtn.addEventListener("click", function () {
+//    hardBtn.classList.remove("selected");
+//    easyBtn.classList.add("selected");
+//    numSquares = 3;
+//    colors = generateRandomColors(numSquares);
+//    pickedColor = pickColor();
+//    colorDisplay.textContent = pickedColor;
+//    for (var i = 0; i < squares.length; i++) {
+//        if (colors[i]) {
+//            squares[i].style.backgroundColor = colors[i];
+//        }
+//        else {
+//            squares[i].style.display = "none";
+//        }
+//    }
+//});
+
+//hardBtn.addEventListener("click", function () {
+//    hardBtn.classList.add("selected");
+//    easyBtn.classList.remove("selected");
+//    numSquares = 6;
+//    colors = generateRandomColors(numSquares);
+//    pickedColor = pickColor();
+//    colorDisplay.textContent = pickedColor;
+//    for (var i = 0; i < squares.length; i++) {
+//        squares[i].style.backgroundColor = colors[i];
+//        squares[i].style.display = "block";
+//    }
+//});
 
 
 resetButton.addEventListener("click", function() {
